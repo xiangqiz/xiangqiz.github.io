d70275d0158a2d92082efab89e4e320dd81cbe23
Xiangqi.Z <15091195370@qq.com>
2018-09-11 21:33:12 +0800

    encode

---
+++ VerificationCode/src/com/main.java
@@ -0,0 +1,14 @@
+package com;
+
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public main(String[] args) {
+		// TODO Auto-generated method stub
+		test test=new test();
+		test.getImg();
+	}
+
+}
